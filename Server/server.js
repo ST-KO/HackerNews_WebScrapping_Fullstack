919d4a51ec7f42fd40165f8d70994f668bfbf2b3
ST-Ko <sithu.ko21120@gmail.com>
2024-07-04 14:55:20 +1000

    update

--- Server/server.js
+++ Server/server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 // Initialise Socket.io
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -25,7 +25,7 @@ const io = new Server(server, {
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
